3ff351cdae95524cc91ded05accb286b7767829e
“[github]” <ruturajjadhav122@gmail.com>
2025-04-01 11:43:24 +0530

    Changed Main name

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Main from "./main";
+import Main from "./Main";
 
 const Nav = () => {
   return (
